7355635170eb1b297d8eeae2ee7488c69dfdd20a
KyleCSharp <111399974+KyleCSharp@users.noreply.github.com>
2022-10-28 14:40:50 -0400

    EX1 finished

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Config
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            #endregion 
+
+            IDbConnection conn = new MySqlConnection(connString);
+            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
+            Console.WriteLine("Hello user, here are the current departments");
+            Console.WriteLine("please press enter. . . . . . .");
+            Console.ReadLine();
+            var depos = repo.GetAllDepartments();
+
+             foreach (var depo in depos)
+             {
+                Console.WriteLine($" ID: {depo.DepartmentID}  NAME : {depo.Name}");
+             }
+
         }
     }
 }
